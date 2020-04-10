1fb0eef886d0f28d384fea0a018941aff07d15bf
yangfn <1240208775@qq.com>
2020-04-10 16:41:43 +0800

    确定属性 增加get set toString

--- src/main/java/com/example/javaee/entity/teacher/Teacher.java
+++ src/main/java/com/example/javaee/entity/teacher/Teacher.java
@@ -19,4 +19,99 @@ public class Teacher {
     int status;
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTeachername() {
+        return teachername;
+    }
+
+    public void setTeachername(String teachername) {
+        this.teachername = teachername;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getClassno() {
+        return classno;
+    }
+
+    public void setClassno(String classno) {
+        this.classno = classno;
+    }
+
+    public String getHeadUrl() {
+        return HeadUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        HeadUrl = headUrl;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", teachername='" + teachername + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                ", college='" + college + '\'' +
+                ", classno='" + classno + '\'' +
+                ", HeadUrl='" + HeadUrl + '\'' +
+                ", path='" + path + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
--- src/main/java/com/example/javaee/entity/user/User.java
+++ src/main/java/com/example/javaee/entity/user/User.java
@@ -11,7 +11,6 @@ public class User {
 
     int id;
     String username;
-
     String password;
     String email;
     String sex;
@@ -22,4 +21,99 @@ public class User {
     int status;
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getHeadUrl() {
+        return HeadUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        HeadUrl = headUrl;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getClassno() {
+        return classno;
+    }
+
+    public void setClassno(String classno) {
+        this.classno = classno;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", sex='" + sex + '\'' +
+                ", HeadUrl='" + HeadUrl + '\'' +
+                ", path='" + path + '\'' +
+                ", classno='" + classno + '\'' +
+                ", college='" + college + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
