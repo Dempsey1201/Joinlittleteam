1024e80386a9daada2b876e8f1b70d68a36dcb78
yanglanq <2873435684@qq.com>
2020-05-18 23:07:55 +0800

    我的班级

--- exam-system-front/src/pages/teacher/views/yoursClass/yoursClass.vue
+++ exam-system-front/src/pages/teacher/views/yoursClass/yoursClass.vue
@@ -1,13 +1,107 @@
 <template>
     <div id="yoursClass">
-        你的班级
+        <div v-show="!currentClass.classname" class="table">
+            <el-table
+                    :data="classList"
+                    style="width: 100%">
+                <el-table-column
+                        type="index"
+                        min-width="50">
+                </el-table-column>
+                <el-table-column
+                        prop="classname"
+                        class="name"
+                        label="班级名称"
+                        min-width="180">
+                </el-table-column>
+                <el-table-column
+                        prop="college"
+                        label="学校"
+                        min-width="180">
+                </el-table-column>
+                <el-table-column
+                        prop="major"
+                        label="科目"
+                        min-width="180"
+                >
+                </el-table-column>
+                <el-table-column
+                        align="right"
+                        min-width="210"
+                >
+                    <template slot="header" slot-scope="scope">
+                        <el-button
+                                style="float: right;display: inline-block"
+                                size="mini"
+                        >Search</el-button>
+                        <el-input
+                                style="float: right;width: 143px"
+                                v-model="search"
+                                size="mini"
+                                placeholder="搜索"/>
+
+                    </template>
+                    <template slot-scope="scope">
+                        <el-button
+                                size="mini"
+                                @click="handleEdit(scope.$index, scope.row)">查看</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+        <checkClass v-if="currentClass.classname" :currentClass="currentClass"
+                    @back="back"
+        ></checkClass>
     </div>
 </template>
 
 <script>
-	export default {
-		name: "yoursClass"
-	}
+    import checkClass from "./checkClass";
+    export default {
+        name: "yoursClass",
+        data(){
+            return{
+                classList:[
+                    {
+                        id:1,
+                        classname:"软件工程",
+                        college:"东北师范大学",
+                        major:"信息科学与技术"
+                    },
+                    {
+                        id:2,
+                        classname:"软件工程",
+                        college:"东北师范大学",
+                        major:"信息科学与技术"
+                    },
+                    {
+                        id:3,
+                        classname:"软件工程",
+                        college:"东北师范大学",
+                        major:"信息科学与技术"
+                    },
+                    {
+                        id:4,
+                        classname:"软件工程",
+                        college:"东北师范大学",
+                        major:"信息科学与技术"
+                    }],
+                currentClass:{},
+                search:""
+            }
+        },
+        methods:{
+            handleEdit(index, row){
+                this.currentClass = row;
+            },
+            back(){
+                this.currentClass = {};
+            }
+        },
+        components:{
+            checkClass
+        }
+    }
 </script>
 
 <style scoped>
